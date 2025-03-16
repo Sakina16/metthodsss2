8bfe06b70563831c6a93d376e4805407e46bdae5
sakina <sakinaea-bb104@code.edu.az>
2025-03-16 17:51:25 +0400

    added new file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//using  System;
+//class Program
+//{
+//    static int Min(int[]array)
+//    {
+//        int min = array[0];
+//        for(int i=1;i<array.Length;i++)
+//        {
+//            if (array[i] < min);
+//            min = array[i];
+//        }
+//        return min;
+//    }
+//}
