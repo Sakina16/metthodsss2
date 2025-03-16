096e604a143620b26a3d1495195f6e021d3162e6
sakina <sakinaea-bb104@code.edu.az>
2025-03-16 19:17:28 +0400

    added two file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿//using  System;
+﻿//task 1
+//using  System;
 //class Program
 //{
 //    static int Min(int[]array)
@@ -12,3 +13,45 @@
 //        return min;
 //    }
 //}
+
+//task 2
+//a
+//using System;
+//using System.Reflection.Metadata.Ecma335;
+//class  Program
+//{
+//    static double Field(double r);
+
+//    {
+//      double pi = 3;
+//      return pi * r * r;
+//}
+//static void Main()
+//{
+//    Console.WriteLine("Field(5)")
+//}
+//b
+//using System;
+//class Program
+//{
+//    static float Field(float a, float b);
+//    {
+//    return a * b;
+//}
+//static void Main();
+//{
+//    Console.WriteLine("Field(1,9)")
+//}
+//c
+//using System;
+//class Program
+//{
+//    static float Field(float a, float b, float c);
+//    {
+//    return 2*(a*b+b*c+a*c);
+//}
+//static void Main();
+//{
+//    Console.WriteLine("Field(1,9,5");
+//}
+//D
